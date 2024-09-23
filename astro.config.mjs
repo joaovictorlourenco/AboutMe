99437f3e7fa5793dd815259e0089dac02a7c55aa
root <root@IRRUBEJD719829.sa.valmont.com>
2024-09-23 13:47:11 -0300

    atualizando pagina para melhorar legibilidade

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  prefetch: true,
+  site: 'https://stargazers.club',
+  integrations: [tailwind(), icon(), sitemap()],
 });
